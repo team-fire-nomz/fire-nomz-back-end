01515d55d6530b49520225410d3dc928ce0b8e26
Manny Dador <EADador@gmail.com>
2022-06-25 15:08:49 -0400

    modified admin view

--- api/admin.py
+++ api/admin.py
@@ -3,5 +3,14 @@ from .models import User, Note, RecipeVersion, TasterFeedback
 
 admin.site.register(User)
 admin.site.register(Note)
-admin.site.register(RecipeVersion)
 admin.site.register(TasterFeedback)
+
+@admin.register(RecipeVersion)
+class MyModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'chef', 'tag_list']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
